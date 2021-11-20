2ef90c6d497826c588ae883951e3b20d4b1d11a8
Christian Onyeneke <onyenekechristian@gmail.com>
2021-11-20 07:37:39 +0100

    Completed Discount API microservice

---
+++ src/Services/Discount/Discount.API/Controllers/DiscountController.cs
@@ -0,0 +1,54 @@
+﻿using Discount.API.Entities;
+using Discount.API.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Discount.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class DiscountController : ControllerBase
+    {
+        private readonly IDiscountRepository _respository;
+
+        public DiscountController(IDiscountRepository respository)
+        {
+            _respository = respository ?? throw new ArgumentNullException(nameof(respository));
+        }
+
+        [HttpGet("{productName}", Name ="GetDiscount")]
+        [ProducesResponseType(typeof(Coupon), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Coupon>> GetDiscount(string productName)
+        {
+            var coupon = await _respository.GetDiscount(productName);
+            return Ok(coupon);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(Coupon), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Coupon>> CreateDiscount([FromBody] Coupon coupon)
+        {
+            await _respository.CreateDiscount(coupon);
+            return CreatedAtRoute("GetDiscount", new { productName = coupon.ProductName }, coupon);
+        }
+
+        [HttpPut]
+        [ProducesResponseType(typeof(Coupon), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Coupon>> UpdateDiscount([FromBody] Coupon coupon)
+        {
+            return Ok(await _respository.UpdateDiscount(coupon));
+        }
+
+        [HttpDelete("{productName}", Name = "DeleteDiscount")]
+        [ProducesResponseType(typeof(void), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Coupon>> DeleteDiscount(string productName)
+        {
+            return Ok(await _respository.DeleteDiscount(productName));
+        }
+    }
+}
